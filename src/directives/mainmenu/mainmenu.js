ae2fe684ba4f56fa46b87587c93be75f3dc268ae
Maxim Chebotarev <maxim-che@yandex.ru>
2016-07-30 14:14:15 +0300

    implement mainmenu directive

--- src/directives/mainmenu/mainmenu.js
+++ src/directives/mainmenu/mainmenu.js
@@ -2,7 +2,17 @@ angular.module('booklya.directives', [])
   .directive('bMainmenu', function() {
     
     return {
-      templateUrl: '/views/mainmenu.html'
+      templateUrl: '/views/mainmenu.html',
+      scope: {
+        collapsed: '='
+      },
+      link: function(scope, element, attrs) {
+
+        element.children('.navbar-toggle').bind('click', function() {
+          scope.collapsed = !scope.collapsed;
+          scope.$apply();
+        });
+      }
     }
   
   });
